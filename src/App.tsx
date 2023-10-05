fdde077eb1686b3d48794eee8feeade4d4f7d8a2
Randy060012 <randy061200@gmail.com>
2023-10-05 14:15:06 +0200

    first react project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import Header from './components/Header'
+import Home from './components/Home'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <Header />
+        <Home></Home>
+        <Footer></Footer>
+      </div>
+    </>
+  )
+}
+
+export default App
